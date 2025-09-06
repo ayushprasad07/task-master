734232b9d1313f75e3031c7cf5ffbe17f707ad00
ayushprasad07 <ayushprasad2110@gmail.com>
2025-09-06 11:14:25 +0530

    Card

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { ArrowBigDown, ArrowDown } from "lucide-react";
 import { Note, YouTube } from "@/model/User";
 import { cn } from "@/lib/utils";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import {
   Card,
   CardAction,
@@ -14,7 +15,7 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 const Dashboard = () => {
   const [youtubeNotes, setYoutubeNotes] = useState<YouTube[]>([]);
@@ -36,7 +37,7 @@ const Dashboard = () => {
   const getNotes = async () => {
     try {
       const result = await axios.get("/api/get-note");
-      console.log("Notes",result.data.data);
+      console.log("Notes", result.data.data);
       setNotes(result.data.notes.slice(0, 3));
     } catch (error) {
       console.log(error);
@@ -88,44 +89,54 @@ const Dashboard = () => {
               <a
                 href={note.url}
                 target="_blank"
-                className="w-full group/card "
+                className="w-full  "
                 key={note._id}
               >
-                <div
-                  className={cn(
-                    "cursor-pointer overflow-hidden relative card rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4 text-neutral-900 aspect-video"
-                  )}
-                  style={{
-                    backgroundImage: `url(${note.preview})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                  }}
-                >
-                  <div className="absolute inset-0 bg-black/20 group-hover/card:backdrop-blur-sm transition duration-300"></div>
-                  <div className="text content ">
-                    <h1 className="font-bold text-lg md:text-xl text-neutral-190 relative z-10">
+                <CardContainer className="inter-var w-full">
+                  <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full  h-auto rounded-xl p-6 border  ">
+                    <CardItem
+                      translateZ="50"
+                      className="text-xl font-bold text-neutral-900 dark:text-white"
+                    >
                       {note.title}
-                    </h1>
-                    <p className="font-normal text-sm text-neutral-190 relative z-10 my-2">
+                    </CardItem>
+                    <CardItem
+                      as="p"
+                      translateZ="60"
+                      className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    >
                       {note.description}
-                    </p>
-                  </div>
-                </div>
+                    </CardItem>
+                    <CardItem translateZ="100" className="w-full mt-4">
+                      <img
+                        src={note.preview}
+                        height="1000"
+                        width="1000"
+                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                        alt="thumbnail"
+                      />
+                    </CardItem>
+                  </CardBody>
+                </CardContainer>
               </a>
             );
           })}
         </div>
       </div>
       {/* Notes */}
-      <div className="w-full mt-10">
+      <div className="w-full mt-10 mb-10">
         <div className="w-full flex justify-between mt-4 items-center">
           <h2 className="text-xl font-semibold">Your Notes</h2>
-          <Link href='/notes' className="font-semibold text-blue-600 flex items-center justify-center">view more <ArrowBigDown className="ml-2"/></Link>
+          <Link
+            href="/notes"
+            className="font-semibold text-blue-600 flex items-center justify-center"
+          >
+            view more <ArrowBigDown className="ml-2" />
+          </Link>
         </div>
-        <hr className="mt-4"/>
+        <hr className="mt-4" />
         <div className="mt-4 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-          {notes.map((note)=>(
+          {notes.map((note) => (
             <Card key={note._id}>
               <CardHeader>
                 <CardTitle>{note.title}</CardTitle>
